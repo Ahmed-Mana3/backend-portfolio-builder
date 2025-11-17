5cc6eae5c9613cb082b7c39c3beacede641689a5
am537@Ahmed_Medhat <you@example.com>
2025-11-17 20:04:59 +0200

    Setup MySQL database connection and builder, user, themes, publish models

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.CustomUser'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio_builder',
+        'USER': 'root',             
+        'PASSWORD': 'asdasdasd.1250', 
+        'HOST': '127.0.0.1',        
+        'PORT': '3306',              
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
 
 
 REST_FRAMEWORK = {
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from builder.models import Portfolio
+from django.utils import timezone
 
-# Create your models here.
+
+class PublishedSite(models.Model):
+    portfolio = models.OneToOneField(Portfolio, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200, unique=True, blank=True)  # final URL
+    is_live = models.BooleanField(default=False)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Automatically generate URL as buildfolio.net/<username>
+        if not self.url:
+            self.url = f"buildfolio.net/{self.portfolio.user.username}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.url
+
+
+
